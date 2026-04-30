49813511c53c58c5994d271de0b17c543db997f6
rmuxnet <armandas.kvietkus@proton.me>
2026-04-30 23:17:28 +0200

    x86/irq: add PS4/Baikal MSI compatibility

--- arch/x86/include/asm/msi.h
+++ arch/x86/include/asm/msi.h
@@ -8,6 +8,7 @@ typedef struct irq_alloc_info msi_alloc_info_t;
 
 int pci_msi_prepare(struct irq_domain *domain, struct device *dev, int nvec,
 		    msi_alloc_info_t *arg);
+void irq_msi_compose_msg(struct irq_data *data, struct msi_msg *msg);
 
 /* Structs and defines for the X86 specific MSI message format */
 
